1caf83f6441f5b704e2d4585387d1102c4e19ddc
Sammyanez <81580171+Sammyanez@users.noreply.github.com>
2021-04-12 22:04:55 -0500

    Added System print

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,7 @@
 public class Main {
 	
 	public static void main(String[] args) {
-		Yahoo myStock = new Yahoo();
-		System.out.println(myStock.getStockPrice("TSLA"));
-
-
-
+	new Dashboard();
 	}
 
 }
